ba0b18cb8dd89b86dc551eea170fe84c53764ff6
Debs <ddurojaiye@gmail.com>
2019-05-27 21:27:52 +0100

    Add intro section and base styles

--- src/App.js
+++ src/App.js
@@ -4,21 +4,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className="header">
+        <h1><span>Doughope</span></h1>
+        <h2>A bread hyrdation calculator</h2>
       </header>
+
+      <div className="main">
+
+        <div className="main-content">
+          <p>In a recipe, the baker's percentage for water is referred to as the "hydration"; it is indicative of the stickiness of the dough and the "crumb" of the bread.</p>
+          <p>
+            Lower hydration rates (e.g., 50–57%) are typical for bagels and pretzels, and medium hydration levels (58–65%) are typical for breads and rolls. Higher hydration levels are used to produce more and larger holes, as is common in artisan breads such as baguettes or ciabatta.
+          </p>
+          <cite>Source:<a href="https://en.wikipedia.org/wiki/Baker_percentage#Dough_hydration">Wikipedia</a></cite>
+
+        </div>
+
+        <aside>
+          <p>
+            [Recipe Summary field]
+          </p>
+        </aside>
+      </div>
+
+
     </div>
   );
 }
